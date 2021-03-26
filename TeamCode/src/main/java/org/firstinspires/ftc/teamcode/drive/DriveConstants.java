bdcdbc98cd3ff763c9f4a84c8bff44d4b8b964c4
dkou <daniel.j.kou@gmail.com>
2021-03-25 19:12:06 -0700

    Fixed PID

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -32,8 +32,8 @@ public class DriveConstants {
      * from DriveVelocityPIDTuner.
      */
     public static final boolean RUN_USING_ENCODER = true;
-    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(35, 0, 16,
-            16);
+    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(40, 0, 15,
+            18);
 
     /*
      * These are physical constants that can be determined from your robot (including the track
