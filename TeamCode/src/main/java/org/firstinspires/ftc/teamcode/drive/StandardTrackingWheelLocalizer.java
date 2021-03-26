d05bd1562cb931e01190bf995d8f4e16b6db808d
Hongbing Kou <hongbing@uber.com>
2021-03-25 19:00:31 -0700

    udpated lateral distance

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
@@ -31,8 +31,8 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double WHEEL_RADIUS = 1; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-    public static double LATERAL_DISTANCE = 14; // in; distance between the left and right wheels
-    public static double FORWARD_OFFSET = -9.5; // in; offset of the lateral wheel
+    public static double LATERAL_DISTANCE = 13.5; // in; distance between the left and right wheels
+    public static double FORWARD_OFFSET = -8.5; // in; offset of the lateral wheel
 
     private Encoder leftEncoder, rightEncoder, frontEncoder;
 
